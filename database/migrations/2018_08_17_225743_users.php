01281e48c4c649f21db51eb85dea71cfe6a77449
gilvan <g.r.almeida@live.com>
2018-08-17 20:26:43 -0300

    add migration users

---
+++ database/migrations/2018_08_17_225743_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email', 100);
+            $table->string('password', 100);
+            $table->date('birthday');
+            $table->char('gender', 1);
+            $table->string('slug', 100);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
