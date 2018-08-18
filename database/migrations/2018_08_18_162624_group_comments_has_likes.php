6f55428863921f9537ebb2e7d61319daba390086
gilvan <g.r.almeida@live.com>
2018-08-18 13:29:47 -0300

    add migration group_posts_has_comments

---
+++ database/migrations/2018_08_18_162624_group_comments_has_likes.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GroupCommentsHasLikes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_comments_has_likes', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('comment_id')->unsigned();
+            $table->string('comment');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('comment_id')
+                ->references('id')
+                ->on('group_posts_has_comments');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropForeign('group_posts_has_comments_comment_id_foreign');
+        Schema::dropIfExists('group_comments_has_likes');
+    }
+}
