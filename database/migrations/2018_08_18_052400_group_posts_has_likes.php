c341f30f0e25d30d1b98ba3fc2040c03769655b8
gilvan <g.r.almeida@live.com>
2018-08-18 02:28:24 -0300

    add migration group_posts_has_likes

---
+++ database/migrations/2018_08_18_052400_group_posts_has_likes.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GroupPostsHasLikes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_posts_has_likes', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('group_has_posts');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropForeign('group_has_posts_post_id_foreign');
+        Schema::dropForeign('users_user_id_foreign');
+        Schema::dropIfExists('group_posts_has_likes');
+    }
+}
