7826ada80090b8c51afe08a4b8f52523b562973f
gilvan <g.r.almeida@live.com>
2018-08-18 19:51:10 -0300

    adding relationships in the user_post model

--- app/Models/Users/UserPost.php
+++ app/Models/Users/UserPost.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models\Users;
 
 use Illuminate\Database\Eloquent\Model;
+use UserPostLike;
+use UserPostComment;
+use UserPostPhoto;
 
 class UserPost extends Model
 {
@@ -12,5 +15,20 @@ class UserPost extends Model
 		'post',
 		'user_id'
 	];
+
+	public function likes()
+	{
+		return $this->hasMany(UserPostLike::class, 'post_id');
+	}
+
+	public function comments()
+	{
+		return $this->hasMany(UserPostComment::class, 'post_id');
+	}
+
+	public function photos()
+	{
+		return $this->hasMany(UserPostPhoto::class, 'post_id');
+	}
 
 }
