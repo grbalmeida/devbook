165c68efe3dd8e2d56fb3713af7661697924fe64
gilvan <g.r.almeida@live.com>
2018-08-18 00:59:25 -0300

    add migration user_posts_has_likes

---
+++ database/migrations/2018_08_18_035458_user_posts_has_likes.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserPostsHasLikes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_posts_has_likes', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('user_has_posts');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropForeign('user_has_posts_post_id_foreign');
+        Schema::dropForeign('users_user_id_foreign');
+        Schema::dropIfExists('user_posts_has_likes');
+    }
+}
