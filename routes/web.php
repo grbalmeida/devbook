9b3ce7c3a15dd0a449b88add491693f49bb04f53
gilvan <g.r.almeida@live.com>
2018-08-19 21:52:32 -0300

    adding profile routes

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,18 @@ Route::prefix('/groups')->group(function() {
 	Route::get('/{group_id}/members', 'GroupController@getMembers')
 		->name('groups.members');
 });
+
+Route::prefix('/{slug}')->group(function($slug) {
+	Route::get('/', 'ProfileController@index')
+		->name('profile.index');
+	Route::get('/about', 'ProfileController@getAbout')
+		->name('profile.about');
+	Route::get('/friends', 'ProfileController@getFriends')
+		->name('profile.friends');
+	Route::get('/groups', 'ProfileController@getGroups')
+		->name('profile.groups');
+	Route::get('/posts/{post_id}', 'ProfileController@getPostById')
+		->name('profile.post');
+	Route::get('/photos', 'ProfileController@getPhotos')
+		->name('profile.photos');
+});	
