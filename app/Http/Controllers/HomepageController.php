b200ff48884a9f10cba93072ab32a5ea678aafd7
gilvan <g.r.almeida@live.com>
2018-08-31 04:35:01 -0300

    Creating the HomepageController with methods to use in homepage view

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    public function index()
+    {
+    	return view('homepage')
+    		->with('days', $this->getDays())
+    		->with('months', $this->getMonths())
+    		->with('years', $this->getYears());
+    }
+
+    public function getDays() 
+    {
+    	$days = [];
+    	for($counter = 1; $counter <= 31; $counter++) 
+    	{
+    		array_push($days, $counter);
+    	}
+    	return $days;
+    }
+
+    public function getMonths()
+    {
+    	$months = [
+    		'Jan', 'Fev', 'Mar',
+    		'Abr', 'Maio', 'Jun',
+    		'Jul', 'Ago', 'Set',
+ 			'Out', 'Nov', 'Dez'
+    	];
+    	return $months;
+    }
+
+    public function getYears()
+    {
+    	$years = [];
+    	for($counter = intval(date('Y')); $counter > 1900; $counter--) {
+    		array_push($years, $counter);
+    	}
+    	return $years;
+    }
+
+}
