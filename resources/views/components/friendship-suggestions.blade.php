b97000bcb488f64a72936600751ee1673b1ac9ae
gilvan <g.r.almeida@live.com>
2018-09-16 00:44:40 -0300

    View with friends suggestions

---
+++ resources/views/components/friendship-suggestions.blade.php
@@ -0,0 +1,17 @@
+<div class="card col-3 mt-2 ml-4">
+	<div class="card-body" data-friendship-suggestions>
+		<div class="h5">Sugestões de amizade</div>
+		<div class="w-100"></div>
+		@foreach($friendshipSuggestions as $friendshipSuggestion)
+		<div class="row">
+			<a href="{{ route('profile.index', $friendshipSuggestion->slug) }}">
+				<img src="{{ url('images/default.jpg') }}" height="40" class="rounded-circle">
+				<span class="db-block ml-3">{{ $friendshipSuggestion->first_name.' '.$friendshipSuggestion->last_name }}</span>
+			</a>
+			<i class="fas fa-times d-block pt-2 ml-5" style="font-size: 20px; cursor: pointer;" data-cancel-friendship-suggestion="{{ $friendshipSuggestion->id }}"></i>
+			<div class="w-100"></div>
+			<button class="btn btn-primary mb-3 mt-3" data-user-suggestion-id="{{ $friendshipSuggestion->id }}">Adicionar</button>
+		</div>
+		@endforeach
+	</div>
+</div>
