ab25cb72b4a36147f53f88928652658aeccd7e4b
gilvan <g.r.almeida@live.com>
2018-09-16 06:11:33 -0300

    Creating methods to remove friend request, and to add friends

--- app/Http/Controllers/Ajax/AddFriendController.php
+++ app/Http/Controllers/Ajax/AddFriendController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Users\User;
 use App\Models\Users\FriendshipRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Users\Relationship;
 
 class AddFriendController extends Controller
 {
@@ -21,5 +22,34 @@ class AddFriendController extends Controller
 			return response('true', 200);
 		}
 		return response('', 200);
-	}    
+	}   
+
+	public function destroy($requestUserId)
+	{
+		$relationship = FriendshipRequest::where('request_user_id', $requestUserId)
+			->where('requested_user_id', Auth::user()->id)->delete();
+		if($relationship) {
+			return response('true', 200);
+		}
+		return response('', 200);
+	}
+
+	public function addFriend($requestUserId)
+	{
+		$relationship = FriendshipRequest::where('request_user_id', $requestUserId)
+			->where('requested_user_id', Auth::user()->id)->delete();
+		$requestUserAddFriend = Relationship::insert([
+			'user_id' => $requestUserId,
+			'friend_id' => Auth::user()->id
+		]);
+		$requestedUserAddFriend = Relationship::insert([
+			'user_id' => Auth::user()->id,
+			'friend_id' => $requestUserId
+		]);
+		if($relationship && $requestUserAddFriend && $requestedUserAddFriend) {
+			return response('true', 200);
+		}
+		return response('', 200);
+	}
+
 }
