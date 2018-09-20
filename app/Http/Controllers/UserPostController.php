1ae5d3ac27a1103ca5a59fd22d183612e5d457d3
gilvan <g.r.almeida@live.com>
2018-09-20 11:15:24 -0300

    Creation of methods to insert the post, and recovery of the last inserted post, to display the last post

---
+++ app/Http/Controllers/UserPostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Users\UserPost;
+use Illuminate\Support\Facades\Auth;
+
+class UserPostController extends Controller
+{
+	public function store($post)
+	{
+		$post = UserPost::create([
+			'post' => $post,
+			'user_id' => Auth::user()->id
+		]);	
+		if($post) {
+			return response($this->getPostById($post->id), 200);
+		}
+		return response('', 200);
+	}
+
+	public function getPostById($id)
+	{
+		$post = UserPost::where('user_has_posts.id', $id)
+			->select('user_has_posts.id', 'user_has_posts.post', 'users.first_name', 'users.last_name')
+			->join('users', 'users.id', '=', 'user_has_posts.user_id')
+			->first();
+		return $post;
+	}
+
+}
